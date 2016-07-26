1d06704cd38056ed39da7aa6d354a9e5bec98ef7
Liz Kerber <emkerber@outlook.com>
2016-07-26 05:59:51 -0500

    Beginning to set-up project

---
+++ client/client.js
@@ -0,0 +1,23 @@
+
+angular.module('catApp', []);
+
+angular.module('catApp').controller('CatController', function($http) {
+
+  var vm = this;
+
+   vm.cats = [];
+   var fetchCats = function() {
+       return $http.get('/cats').then(function(response){
+           if(response.status !== 200){
+               throw new Error('Failed to fetch cats from the API');
+           }
+           vm.cat = {};
+           vm.cats = response.data;
+           return response.data;
+       })
+   };
+   vm.add = function(cat){
+       return $http.post('/add', cat).then(fetchCats());
+   };
+   fetchCats();
+});
